58e576e08a096ad2fc487ab2b724d88cfb80065d
16jalayt <16jalayt@gmail.com>
2019-09-20 16:01:46 -0500

    fixed int issue

--- src/Scanner.java
+++ src/Scanner.java
@@ -208,10 +208,18 @@ public class Scanner
                 token.value = tokenSoFar;
             }
             else {
-                tokenSoFar += prog.charAt(pos);
+                //tokenSoFar += prog.charAt(pos);
                 switch(prog.charAt(pos)) {
                     case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
-                        pos++; state3(); break;
+                        tokenSoFar += prog.charAt(pos); pos++; state3(); break;
+                    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': case 'h':
+                    case 'i': case 'j': case 'k': case 'l': case 'm': case 'n': case 'o': case 'p':
+                    case 'q': case 'r': case 's': case 't': case 'u': case 'v': case 'w': case 'x':
+                    case 'y': case 'z':
+                    case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': case 'H':
+                    case 'I': case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P':
+                    case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W': case 'X':
+                    case 'Y': case 'Z': token.type = Token.Type.Integer; token.value = tokenSoFar; break;
                     default: pos++; state60();
                 }
             }
