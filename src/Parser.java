a20484c7e89154458fe6ce946ca8a73787463f11
Jason Layton <16jalayt@gmail.com>
2019-10-02 12:17:45 -0500

    worked on parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,26 +1,97 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.*;
 
 public class Parser
-{   //TODO: on parole
+{   //https://codingexplained.com/coding/java/enum-to-integer-and-integer-to-enum
     private enum Rule
     {
         NULL, PROGRAM, DEFINITIONS, DEF, FORMALS, NONEMPTYFORMALS, NEFREST, FORMAL, BODY, PRINTBODY,
         TYPE, EXPR, EXPRREST, SIMPLEEXPR, SIMPLEEXPRREST, TERM, TERMREST, FACTOR, NOTFACTOR, NEGFACTOR,
         IDENTIFIERACTUALS, ACTUALS, NONEMPTYACTUALS, NEAREST, LITERAL, PRINTSTATEMENT;
+
+        private int value;
+        private int count = 100;
+        private static Map map = new HashMap<>();
+
+        private Rule() {
+            this.value = count;
+            count++;
+        }
+
+        static
+        {
+            for (Rule rule : Rule.values())
+            {
+                map.put(rule.value, rule);
+            }
+        }
+
+        public static Rule valueOf(int rule)
+        {
+            return (Rule) map.get(rule);
+        }
+        public static int length()
+        {
+            return map.size();
+        }
+        public static boolean contains(int obj)
+        {
+            if(map.containsValue(obj))
+                return true;
+            else
+                return false;
+        }
+        public int getValue() {
+            return value;
+        }
     }
     //TODO:make all terminals
     //make into arraylist
     private enum Terminals
     {
         NULL, print, NUMBER, BOOLEAN, not, and, or, IDENTIFIER, leftParen, rightParen, plus, minus,
-        equals, lessThan, colon, comma, If, then, Else, integer, Boolean, function;
+        equals, lessThan, colon, comma, If, then, Else, integer, Boolean, function, EOF;
+
+        private int value;
+        private int count = 200;
+        private static Map map = new HashMap<>();
+
+        private Terminals() {
+            this.value = count;
+            count++;
+        }
+
+        static
+        {
+            for (Terminals terminals : Terminals.values())
+            {
+                map.put(terminals.value, terminals);
+            }
+        }
+
+        public static Terminals valueOf(int terminals)
+        {
+            return (Terminals) map.get(terminals);
+        }
+        public static int length()
+        {
+            return map.size();
+        }
+        public static boolean contains(int obj)
+        {
+            if(map.containsValue(obj))
+                return true;
+            else
+                return false;
+        }
+        public int getValue() {
+            return value;
+        }
     }
-    ArrayList<String> terminal = new ArrayList<String>(Arrays.asList(new String[]
-            {"print", "("}));
-    ArrayList<String> nonterminal = new ArrayList<String>(Arrays.asList(new String[]
-            {"program", "definitions"}));
+    //ArrayList<String> terminal = new ArrayList<String>(Arrays.asList(new String[]
+            //{"print", "("}));
+    //same as enum Rule above
+    //ArrayList<String> nonterminal = new ArrayList<String>(Arrays.asList(new String[]
+            //{"program", "definitions"}));
 
     public Rule currentRule;
     //public Terminals terminals;
@@ -47,46 +118,49 @@ public class Parser
     {
         //Token tok;
         //tmp. need default though
-        currentRule = Rule.Expression;
+        //currentRule = Rule.Expression;
         int nextFunc = 0;
         //cant be type token because can be nonterminal
-        Stack<Token> stack = new Stack<>();
-        stack.push(new Token(Token.Type.EOF,"null"));
+        Stack<Integer> stack = new Stack<>();
+        stack.push(Terminals.EOF.getValue());
         Token next = new Token(Token.Type.Error,"");
         while (!stack.isEmpty())
         {
-            Token temp = stack.peek();
+            int temp = stack.peek();
             //if tmp is terminal
-            if(terminal.contains(temp.value))
+            if(Terminals.contains(temp))
             {
                 next = scan.next();
-                if(temp.type == next.type)
+                //if(temp == next.type.ordinal())
+                //////////////////line where i get confused
+                if(temp == Terminals.contains(next.type.ordinal()))
                     stack.pop();
                 else
                 {
-                    System.out.println("Token mismatch error: " + temp.type.toString() + " and: " + next.type.toString());
+                    System.out.println("Token mismatch error: " + temp + " and: " + next.type.toString());
                     return false;
                 }
             }
-            else if (nonterminal.contains(temp.value))
+            //else if (nonterminal.contains(temp))
+            else if (Rule.contains(temp))
             {
                 next = scan.peek();
-                int rule = table[temp][next.type];
-                if(rule != -1)
+                currentRule = Rule.valueOf(table[temp][next.type.ordinal()]);
+                if(currentRule.ordinal() != -1)
                 {
                     stack.pop();
-                    stack.push(rule);
+                    stack.push(currentRule.ordinal());
                 }
                 else
                 {
-                    System.out.println("Can not expand: " + temp.type.toString() + " on: " + next.type.toString());
+                    System.out.println("Can not expand: " + temp + " on: " + next.type.toString());
                     return false;
                 }
 
             }
             else
             {
-                System.out.println("Invalid item on stack: " + temp.type.toString());
+                System.out.println("Invalid item on stack: " + temp);
                 return false;
             }
         }
