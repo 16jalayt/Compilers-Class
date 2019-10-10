82f2f2268bdf27e81591c0aa257efb20a6789686
Jason Layton <16jalayt@gmail.com>
2019-10-09 20:53:31 -0500

    Scanner test is currently broken

--- src/tests/ScannerTest.java
+++ src/tests/ScannerTest.java
@@ -5,15 +5,66 @@ import org.junit.jupiter.api.Test;
 import src.Parser;
 import src.Scanner;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScannerTest
 {
     @Test
-    public void empty()
+    public void empty() throws Exception
     {
-        Scanner scan = new Scanner("");
-        Assertions.assertEquals( "EOF     ", scan.next().toString());
+        Assertions.assertEquals(
+                "Comment     \n" +
+                "Keyword     function\n" +
+                "Identifier     test1\n" +
+                "Punctuation     (\n" +
+                "Identifier     var\n" +
+                "Punctuation     :\n" +
+                "Keyword     boolean\n" +
+                "Punctuation     )\n" +
+                "Punctuation     :\n" +
+                "Keyword     boolean\n" +
+                "Keyword     if\n" +
+                "Identifier     var\n" +
+                "Punctuation     =\n" +
+                "Identifier     true\n" +
+                "Punctuation     +\n" +
+                "Punctuation     -\n" +
+                "Punctuation     -\n" +
+                "Punctuation     +\n" +
+                "Identifier     true\n" +
+                "Keyword     then\n" +
+                "Identifier     print\n" +
+                "Punctuation     (\n" +
+                "Identifier     true\n" +
+                "Punctuation     )\n" +
+                "Keyword     else\n" +
+                "Identifier     false\n" +
+                "EOF     "
+                , print("./examples/klein-programs/circular-prime.kln"));
+    }
+
+    private String print(String fileName) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+        String currentLine;
+        String fullFile = "";
+        //do pretty printing here
+        while ((currentLine = br.readLine()) != null)
+            //for some reason, concat removes newline. hack to fix issues. Should be removed down the line anyway
+            fullFile += currentLine + '\n';
+        Scanner scan = new Scanner(fullFile);
+        String out = new String();
+        System.out.println(scan.peek().toString());
+        while (scan.peek().type != src.Token.Type.EOF && scan.peek().type != src.Token.Type.Error)
+            out  =  scan.next().toString();
+        //one last call to display EOF
+        out += scan.next().toString();
+        System.out.println(out);
+        return out;
     }
 
 
