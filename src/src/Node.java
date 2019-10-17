48434b2d5ff47e9f121b1c03103fa2c46dfbb261
andrewlitterer <55213161+andrewlitterer@users.noreply.github.com>
2019-10-16 20:18:44 -0500

    Added names to nodes

--- src/src/Node.java
+++ src/src/Node.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Node
 {
     public Node parent;
+    public String name;
     public List<Node> children = new LinkedList<Node>();
     //need empty as default
     public Node()
@@ -26,19 +27,30 @@ public class Node
     }
     public static class Program extends Node
     {
+        public Program() {
 
+            this.name = "Program";
+        }
     }
     public static class Definitions extends Node
     {
-        public Definitions(Node[] children) { super(children); }
+        public Definitions(Node[] children) {
+            super(children);
+            this.name = "Definitions";
+        }
     }
     public static class Identifier extends Node
     {
-        public Identifier(Node[] children) { super(children); }
+        public Identifier(Node[] children) {
+            super(children);
+            this.name = "Identifier";
+        }
     }
     public static class Formals extends Node
     {
-        public Formals(Node[] children) { super(children); }
+        public Formals(Node[] children) { super(children);
+            this.name = "Formals";
+        }
     }
     public static class Integer extends Node
     {
@@ -48,6 +60,7 @@ public class Node
         {
             super(children);
             this.value = value;
+            this.name = "Integer";
         }
     }
     public static class Boolean extends Node
@@ -58,6 +71,7 @@ public class Node
         {
             super(children);
             this.value = value;
+            this.name = "Boolean";
         }
     }
     public static class Binary extends Node
@@ -68,6 +82,7 @@ public class Node
         {
             super(children);
             this.value = value;
+            this.name = "Binary";
         }
     }
     public static class Unary extends Node
@@ -78,19 +93,29 @@ public class Node
         {
             super(children);
             this.value = value;
+            this.name = "Unary";
         }
     }
     public static class If extends Node
     {
-        public If(Node[] children) { super(children); }
+        public If(Node[] children) {
+            super(children);
+            this.name = "If";
+        }
     }
     public static class Actuals extends Node
     {
-        public Actuals(Node[] children) { super(children); }
+        public Actuals(Node[] children) {
+            super(children);
+            this.name = "Actuals";
+        }
     }
     public static class FunctionCall extends Node
     {
-        public FunctionCall(Node[] children) { super(children); }
+        public FunctionCall(Node[] children) {
+            super(children);
+            this.name = "FunctionCalls";
+        }
     }
 
     @Override
