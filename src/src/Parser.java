85bb756a18aeee33f8fcc0ecb7fe37317193350c
andrewlitterer <55213161+andrewlitterer@users.noreply.github.com>
2019-10-25 12:16:19 -0500

    Prints multiple AST's now

--- src/src/Parser.java
+++ src/src/Parser.java
@@ -45,7 +45,7 @@ public class Parser
     private final int tableX = 30;
     private final int tableY = 30;
 
-    private Node tree = new Node();
+    private Node tree = new Node.Program();
 
 
     public void getTree() {
@@ -160,13 +160,11 @@ public class Parser
                 switch(temp) {
                     //there is static prog node at top, need to set that
                     case "make-<PROGRAM>":
-                        Node programNode = new Node.Program();
                         System.out.println("PROGRAM");
                         System.out.println("NodeStack: " + NodeStack);
-                        programNode.children.add(NodeStack.pop());
-                        tree.children.add(programNode);
-                        //programNode keeps getting overwritten, thus only the last function in a file is made into AST
-
+                        while(NodeStack.peek().name.equals("Def")) {
+                            tree.children.add(NodeStack.pop());
+                        }
                         return true;
                     case "make-<DEF>":
                         if(Main.debugStage == 6) System.out.println("NodeStack: " + NodeStack);
