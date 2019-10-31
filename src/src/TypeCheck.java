8b0960cc96c4e6b3207676d1d02d763eb81ac9d1
andrewlitterer <55213161+andrewlitterer@users.noreply.github.com>
2019-10-30 22:58:30 -0500

    Added to type checker, added error generator

--- src/src/TypeCheck.java
+++ src/src/TypeCheck.java
@@ -5,91 +5,56 @@ import java.util.*;
 public class TypeCheck {
     Map<String, TT_Obj> stable = new HashMap<String, TT_Obj>();
     //stable.put("func", new TT_Obj());
+    String errorCode = "";
 
     public void traversalHelper(Node tree)
+    {
+        if (tree.name.equals("Body")){
+            traversal(tree);
+        }
+        else if (tree.name.equals("Formals")){
+            formalsTraversal(tree);
+        }
+        else if (tree.name.equals("Program")) {
+            for (int i = 0; i < tree.children.size(); i++) {
+                Node def = tree.children.get(i);
+                traversalHelper(def);
+            }
+        }
+        else if (tree.name.equals("Def")){
+            //no idea how to make child#2 into a TT_Obj type
+            //stable.put(tree.children.get(0).value.toString(), tree.children.get(2).value);
+            //formals
+            traversalHelper(tree.children.get(1));
+            //body
+            traversalHelper(tree.children.get(3));
+        }
+    }
+
+    public void formalsTraversal(Node tree)
     {
         for (int i=0; i < tree.children.size(); i++)
         {
             Node child = tree.children.get(i);
-            if (child.name.equals("Body")){
-                traversal(child);
-            }
-            else{
-                traversalHelper(child);
-                //add function name and formals to symbol table here
-            }
+            //Not sure how to make second child into a TT_Obj
+            //stable.put(child.children.get(0).value.toString(), child.children.get(1).value);
         }
-        //add coce for function anme an formals here
     }
 
     public void traversal(Node tree)
     {
-        //traverse down AST
         for (int i=0; i < tree.children.size(); i++)
         {
             Node child = tree.children.get(i);
             traversal(child);
         }
         compareChildrenNodes(tree);
-
-        //begin type checking
-
-        //when we reach body, return its type
-        /*if (tree.name.equals("Body")) {
-            return nodeType.get(0);
-        }
-        else if (tree.name.equals("BooleanValue") || tree.name.equals("boolean")) {
-            return "Boolean";
-        }
-        else if (tree.name.equals("Number") || tree.name.equals("integer")) {
-            return "Integer";
-        }
-        else if (tree.name.equals("Identifier")) {
-            //look up tree's type in Symbol Table
-        }
-        else if (tree.name.equals("Expr")) {
-            return nodeType.get(0);
-        }
-        else if (tree.name.equals("If")) {
-            boolean ifStatement = false;
-            boolean thenElse = false;
-            if (nodeType.get(0).equals("Boolean")){
-                ifStatement = true;
-            }
-            if (nodeType.get(1).equals(nodeType.get(2))){
-                thenElse = true;
-            }
-            if (ifStatement && thenElse){
-                return nodeType.get(1);
-            }
-            else
-                return "Error: then/else types do not match";
-        }
-        else if (tree.name.equals("Binary")) {
-            //Do binary comparison here
-        }
-        else if (tree.name.equals("Unary")) {
-            //do Unary comparison here
-        }
-        else if (tree.name.equals("Actuals")) {
-            //do something with actuals, look up in symbol table
-        }
-        else if (tree.name.equals("FunctionCalls")) {
-            //do something with function call, look up in symbol table
-        }
-        else
-            return "Parser error";
-
-        //if it gets this far, there was an error
-        return "Type Check Error";*/
     }
-
-
 
 
     public boolean check(Node tree)
     {
-        //need to do dfs down to leaf then perculate the return types back up, then build symbol table
+        //need to do dfs down to leaf then percolate the return types back up, then build symbol table
         //pre order traversal
         System.out.println("Type check");
         return true;
@@ -106,21 +71,28 @@ public class TypeCheck {
             compareIfNode(currentNode); }
         else if (currentNode.name.equals("Expr")) {
             compareExprNode(currentNode); }
-        else if (currentNode.name.equals("Number") || currentNode.name.equals("integer")) {
-            compareNumberNode(currentNode); }
-        else if (currentNode.name.equals("BooleanValue") || currentNode.name.equals("boolean")) {
-            compareBooleanNode(currentNode); }
         else if (currentNode.name == "Identifier") {
             compareIdentifierNode(currentNode); }
         else if (currentNode.name == "FunctionCalls") {
             compareFunctionCallsNode(currentNode); }
         else {
-            // I don't think this should ever be hit
-            // we should be already returning once a node
-            // has not children. If this happens there probably
-            // is in error in the code.
-
+            // We would hit this if our node is a literal, since those already have their type
+            //preset, so we would do nothing. Do we even need an else clause?
         }
+    }
+
+    public void compareBodyNode(Node body){
+        //Added a for loop in case of print statement, might not be needed though
+        for (int i=0; i < body.children.size(); i++) {
+            Node child = body.children.get(i);
+            if (child.name.equals("Expr")) {
+                body.type = child.type;
+            }
+        }
+    }
+
+    public void compareExprNode(Node expr){
+        expr.type = expr.children.get(0).type;
     }
 
     //Does a comparison for all types of unary nodes by looking into
@@ -130,12 +102,14 @@ public class TypeCheck {
             if(unaryNode.children.get(0).type == "boolean") {
                 unaryNode.type = unaryNode.children.get(0).type; }
             else {
+                unaryNode.type = "Error";
                 generateError(unaryNode); }
         }
         else {
             if(unaryNode.children.get(0).type == "integer"){
                 unaryNode.type = unaryNode.children.get(0).type; }
             else {
+                unaryNode.type = "Error";
                 generateError(unaryNode);
             }
         }
@@ -146,11 +120,11 @@ public class TypeCheck {
     // Then it looks at if the child node is right for the parent's op
     public void binaryCompareNodes(Node binaryNode){
         if(!compare2Nodes(binaryNode.children.get(0), binaryNode.children.get(1))){
+            binaryNode.type = "Error";
             generateError(binaryNode); }
         else {
             compareBinaryOpToType(binaryNode); }
     }
-
 
     public boolean compare2Nodes(Node expr1, Node expr2){
         if(expr1.type.equals(expr2.type)){
@@ -169,6 +143,7 @@ public class TypeCheck {
             if (binaryNode.children.get(0).type == "boolean"){
                 binaryNode.type = binaryNode.children.get(0).type; }
             else {
+                binaryNode.type = "Error";
                 generateError(binaryNode); }
         }
         else {
@@ -176,17 +151,19 @@ public class TypeCheck {
                 binaryNode.type = binaryNode.children.get(0).type;
             }
             else {
+                binaryNode.type = "Error";
                 generateError(binaryNode); }
         }
     }
 
-
     public void compareIfNode(Node If){
         if(If.children.get(0).type != "boolean" ){
+            If.type = "Error";
             generateError(If); }
         else if(compare2Nodes(If.children.get(1),If.children.get(2))){
             If.type = If.children.get(1).type;}
         else {
+            If.type = "Error";
             generateError(If); }
     }
 
@@ -197,6 +174,7 @@ public class TypeCheck {
             Identifier.type = stable.get(Identifier.value.toString()).toString();
         }
         else{
+            Identifier.type = "Error";
             generateError(Identifier);
         }
     }
@@ -218,6 +196,28 @@ public class TypeCheck {
         return true;
     }
 
+    public void generateError(Node error){
+        //Creates a long list of errors, tracing back to the original node which caused the first error
+        String temp;
+        if (errorCode.equals("")) {
+            if (error.value != null) {
+                errorCode = "Error at " + error.name + "Node, with value of " + error.value;
+            }
+            else {
+                errorCode = "Error at " + error.name + "Node";
+            }
+        }
+        else{
+            if (error.value != null) {
+                temp = "Error at " + error.name + "Node, with value of " + error.value + ", caused by: ";
+            }
+            else {
+                temp = "Error at " + error.name + "Node, caused by: ";
+            }
+            errorCode = temp + errorCode;
+        }
+    }
+
     public void printSymbolTable()
     {
         Set< Map.Entry< String,TT_Obj> > set = stable.entrySet();
@@ -227,9 +227,4 @@ public class TypeCheck {
             System.out.println(stable.getKey()+":"+stable.getValue());
         }
     }
-
-
-
-
-
 }
