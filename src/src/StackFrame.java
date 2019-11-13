6acf9bcc13b575cd2efeb53ec2a8db2bd297c0da
16jalayt <16jalayt@gmail.com>
2019-11-13 09:55:58 -0600

    Work on stack frames

--- src/src/StackFrame.java
+++ src/src/StackFrame.java
@@ -1,5 +1,6 @@
 package src;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,14 +19,28 @@ public class StackFrame
 
     }*/
 
-    public static void saveRegisters()
+    public static void saveRegisters() throws IOException
     {
         //See save register in asm staging.txt
+        Generator.print("ldc", 6,1,6,"inc top by 1");
+        Generator.print("st", 1,0,6,"store r1 at top in dmem");
+        Generator.print("ldc", 6,1,6,"inc top by 1");
+        Generator.print("st", 2,0,6,"store r2 at top in dmem");
+        Generator.print("ldc", 6,1,6,"inc top by 1");
+        Generator.print("st", 3,0,6,"store r3 at top in dmem");
+        Generator.print("ldc", 6,1,6,"inc top by 1");
+        Generator.print("st", 4,0,6,"store r4 at top in dmem");
     }
 
-    public static void makeFrame()
+    public static void makeFrame() throws IOException
     {
         //See make frame in asm staging.txt
+
+        //!Not quite right, would be current pos, need to sub some
+        Generator.print("ldc", 6,1,6,"inc top by 1");
+        Generator.print("st", 7,-2,6,"store pc at top in dmem");
         saveRegisters();
+        //save arguments, inc top
+        //
     }
 }
